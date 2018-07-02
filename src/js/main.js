5e5b66f6b007dfa22cf05df121f40fd7431f6446
DudkinON <dudkin.on@gmail.com>
2018-07-02 04:46:30 -0500

    feat: set default style as CSS

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ require("babel-runtime/regenerator");
 require("react-hot-loader/patch");
 require("babel-register");
 require("webpack-hot-middleware/client?reload=true");
-require("../styles/main.styl");
+require("../styles/main.css");
 require("../html/index.html");
 require("./app/app");
