177ec51b3cd632afa2c889bcda5bbbc522069dea
DudkinON <dudkin.on@gmail.com>
2018-07-02 03:42:09 -0500

    feat: Add webpack entry main file

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+require("babel-runtime/regenerator");
+require("react-hot-loader/patch");
+require("babel-register");
+require("webpack-hot-middleware/client?reload=true");
+require("../styles/main.styl");
+require("../html/index.html");
+require("./app/app");
